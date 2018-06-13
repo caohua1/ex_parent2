186af68c691d0591c0e5424853f257d2cd92715f
王辉 <wh13552705269@163.com>
2018-06-13 18:10:31 +0800

    1:商家入驻

--- ex-core/src/main/java/com/ex/controller/merchant_app_controller/login/BusinessLicenseInfoController.java
+++ ex-core/src/main/java/com/ex/controller/merchant_app_controller/login/BusinessLicenseInfoController.java
@@ -2,15 +2,20 @@ package com.ex.controller.merchant_app_controller.login;
 
 import com.ex.entity.BusinessLicenseInfo;
 import com.ex.service.BusinessLicenseInfoService;
+import com.ex.util.BaiduOcrtools;
+import com.ex.util.FileUploadTool;
 import com.ex.util.JsonView;
 import com.ex.util.PageRequest;
+import com.ex.vo.FileEntity;
 import com.github.pagehelper.PageInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.text.SimpleDateFormat;
 
 @RestController
@@ -23,9 +28,32 @@ public class BusinessLicenseInfoController {
     @Autowired
     private BusinessLicenseInfoService businessLicenseInfoService;
 
-    @RequestMapping("/all")
-    public JsonView test(PageRequest page){
+    @RequestMapping("/upload")
+    public JsonView insertBusinessLicenseInfo(MultipartFile[] files, MultipartFile idCardImage, MultipartFile businessImage, HttpServletRequest request, BusinessLicenseInfo businessLicenseInfo) {
+        try {
+            int code = businessLicenseInfoService.insertBusinessLicenseInfo(files,idCardImage,businessImage,request,businessLicenseInfo);
+            if(code==1001){
+                return JsonView.fail("提交失败，请重新上传!");
+            }else if(code==1002){
+                return  JsonView.fail("营业执照有遮挡，请重新上传!");
+            }else if(code==1003){
+                return  JsonView.fail("身份证模糊，请重新上传!");
+            }
+            return JsonView.success("提交成功!");
+        }catch (Exception e){
+            e.printStackTrace();
+            return JsonView.fail(JsonView.EXPIRED,"服务器错误!");
+        }
+    }
 
+    /**
+     * 获取所有认证信息
+     *
+     * @param page
+     * @return
+     */
+    @RequestMapping("/all")
+    public JsonView findAll(PageRequest page) {
         try {
             logger.info("Request comming to find user list...");
             PageInfo<BusinessLicenseInfo> pageInfo = businessLicenseInfoService.findByPage(page);
--- ex-core/src/main/java/com/ex/service/impl/BusinessLicenseInfoServiceImpl.java
+++ ex-core/src/main/java/com/ex/service/impl/BusinessLicenseInfoServiceImpl.java
@@ -4,12 +4,16 @@ import com.ex.dao.BusinessLicenseInfoDao;
 import com.ex.entity.BusinessLicenseInfo;
 import com.ex.service.BusinessLicenseInfoService;
 import com.ex.util.BaiduOcrtools;
+import com.ex.util.FileUploadTool;
 import com.ex.util.PageRequest;
+import com.ex.vo.FileEntity;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,6 +26,7 @@ public class BusinessLicenseInfoServiceImpl implements BusinessLicenseInfoServic
 
     /**
      * 查询所有身份证和营业执照信息
+     *
      * @return
      */
     @Override
@@ -35,17 +40,24 @@ public class BusinessLicenseInfoServiceImpl implements BusinessLicenseInfoServic
 
     /**
      * 添加身份证和营业执照信息
-     * @param idCardPath
-     * @param charterPicUrl
+     *
+     * @param files
+     * @param request
      * @param businessLicenseInfo
      * @return
      */
     @Override
-    public int insertBusinessLicenseInfo(String idCardPath, String charterPicUrl, BusinessLicenseInfo businessLicenseInfo) {
+    public int insertBusinessLicenseInfo(MultipartFile[] files, MultipartFile idCardImage, MultipartFile businessImage, HttpServletRequest request, BusinessLicenseInfo businessLicenseInfo) {
+        FileEntity entity = new FileEntity();
+        FileUploadTool fileUploadTool = new FileUploadTool();
         Map<String, String> businessMap;
+
         try {
-            if (charterPicUrl != null) {
-                businessMap = BaiduOcrtools.business(charterPicUrl);
+            /////////////////
+            if (businessImage != null) {
+                entity = fileUploadTool.createFile(businessImage, request);
+                System.out.println("返回报文---" + entity);
+                businessMap = BaiduOcrtools.business(entity.getPath());
                 businessLicenseInfo.setCompanyname(businessMap.get("单位名称"));
                 businessLicenseInfo.setLegalperson(businessMap.get("法人"));
                 businessLicenseInfo.setSocialcreditcode(businessMap.get("社会信用代码"));
@@ -53,36 +65,80 @@ public class BusinessLicenseInfoServiceImpl implements BusinessLicenseInfoServic
                 businessLicenseInfo.setCompanyaddress(businessMap.get("地址"));
                 businessLicenseInfo.setMerchantidnumber(businessMap.get("证件编号"));
                 businessLicenseInfo.setEstablishmentdate(businessMap.get("成立日期"));
+                if (businessLicenseInfo.getCompanyname().equals("无") ||
+                        businessLicenseInfo.getLegalperson().equals("无") ||
+                        businessLicenseInfo.getSocialcreditcode().equals("无") ||
+                        businessLicenseInfo.getValidityperiod().equals("无") ||
+                        businessLicenseInfo.getCompanyaddress().equals("无") ||
+                        businessLicenseInfo.getMerchantidnumber().equals("无") ||
+                        businessLicenseInfo.getEstablishmentdate().equals("无")) {
+                    // 1002营业执照有遮挡
+                    return 1002;
+                }
+            } else {
+                //1001图片上传失败
+                return 1001;
             }
-            if (idCardPath != null) {
-                businessMap = BaiduOcrtools.idCard(idCardPath);
-                businessLicenseInfo.setRealname(businessMap.get("姓名"));
-                businessLicenseInfo.setIdcard(businessMap.get("公民身份号码"));
-                businessLicenseInfo.setBirthday(businessMap.get("出生"));
-                businessLicenseInfo.setSex(businessMap.get("性别") == "男" ? 1 : 0);
-                businessLicenseInfo.setAddress(businessMap.get("地址"));
-                businessLicenseInfo.setNational(businessMap.get("民族"));
+            if (idCardImage != null) {
+                entity = fileUploadTool.createFile(idCardImage, request);
+                if (entity != null) {
+                    System.out.println("返回报文---" + entity);
+                    businessMap = BaiduOcrtools.idCard(entity.getPath());
+                    businessLicenseInfo.setRealname(businessMap.get("姓名"));
+                    businessLicenseInfo.setIdcard(businessMap.get("公民身份号码"));
+                    businessLicenseInfo.setBirthday(businessMap.get("出生"));
+                    businessLicenseInfo.setSex(businessMap.get("性别") == "男" ? 1 : 0);
+                    businessLicenseInfo.setAddress(businessMap.get("地址"));
+                    businessLicenseInfo.setNational(businessMap.get("民族"));
+                    if (businessLicenseInfo.getRealname().equals("无") ||
+                            businessLicenseInfo.getIdcard().equals("无") ||
+                            businessLicenseInfo.getBirthday().equals("无") ||
+                            businessLicenseInfo.getSex().equals("无") ||
+                            businessLicenseInfo.getAddress().equals("无") ||
+                            businessLicenseInfo.getNational().equals("无")) {
+                        // 1003身份证有遮挡
+                        return 1003;
+                    }
+                } else {
+                    return 1001;
+                }
+                //////////////////////////////
+                for (int i = 0; 1 < files.length; i++) {
+                    if (i == 0) {
+
+                    } else {
+                        entity = fileUploadTool.createFile(files[i], request);
+                        if (entity == null) {
+                            System.out.println("返回报文---" + entity);
+                        } else {
+                            return 1001;
+                        }
+                    }
+                }
+                businessLicenseInfoDao.insertBusinessLicenseInfo(businessLicenseInfo);
+
             }
-            businessLicenseInfoDao.insertBusinessLicenseInfo(businessLicenseInfo);
-            return 1;
+            return 200;
         } catch (Exception e) {
             e.printStackTrace();
+            return 1001;
         }
-        return 0;
     }
 
-    /**
-     * 按条件查询
-     * @param businessLicenseInfo
-     * @return
-     */
-    @Override
-    public List<BusinessLicenseInfo> byConditionsQuery(BusinessLicenseInfo businessLicenseInfo) {
-        try {
-            return businessLicenseInfoDao.byConditionsQuery(businessLicenseInfo);
-        }catch (Exception e){
-            e.printStackTrace();
+        /**
+         * 按条件查询
+         *
+         * @param businessLicenseInfo
+         * @return
+         */
+        @Override
+        public List<BusinessLicenseInfo> byConditionsQuery (BusinessLicenseInfo businessLicenseInfo){
+            try {
+                return businessLicenseInfoDao.byConditionsQuery(businessLicenseInfo);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
         }
-        return null;
-    }
+
 }
