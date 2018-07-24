441319fb8983e650fd08813c332c6f85d1d831b5
Tianzihao <15383743389@163.com>
2018-07-24 11:38:54 +0800

    后台运营享约模块

--- ex-core/src/main/java/com/ex/service/AppointmentMeetService.java
+++ ex-core/src/main/java/com/ex/service/AppointmentMeetService.java
@@ -2,8 +2,10 @@ package com.ex.service;
 
 import com.ex.util.PageRequest;
 import com.ex.vo.AppointmentMeetVo;
+import com.ex.vo.FriendVo;
 import com.github.pagehelper.PageInfo;
 
+import java.util.List;
 import java.util.Map;
 
 public interface AppointmentMeetService {
@@ -11,4 +13,8 @@ public interface AppointmentMeetService {
     public PageInfo<AppointmentMeetVo> selectAppointmentMeetVo(PageRequest page,Map staMap);
 
     public PageInfo<AppointmentMeetVo> applyForAppointmentMeetVo(PageRequest page,Map staMap);
+
+    public PageInfo<FriendVo> selectFriend(PageRequest page,Long merchantId);
+
+    public PageInfo<FriendVo> selectFriendAdd(PageRequest page,Long merchantId);
 }
--- ex-core/src/main/java/com/ex/service/impl/AppointmentMeetServiceimpl.java
+++ ex-core/src/main/java/com/ex/service/impl/AppointmentMeetServiceimpl.java
@@ -4,6 +4,7 @@ import com.ex.dao.AppointmentMeetDao;
 import com.ex.service.AppointmentMeetService;
 import com.ex.util.PageRequest;
 import com.ex.vo.AppointmentMeetVo;
+import com.ex.vo.FriendVo;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,5 +43,32 @@ public class AppointmentMeetServiceimpl implements AppointmentMeetService {
         List<AppointmentMeetVo> appointmentMeetVos = appointmentMeetDao.selectAppointmentMeetVo(staMap);
         PageInfo<AppointmentMeetVo> appointmentMeetVoPageInfo = new PageInfo<>(appointmentMeetVos);
         return appointmentMeetVoPageInfo;
+    }
+
+    /**
+     * PC端查询我加的享友
+     * @param page
+     * @param merchantId
+     * @return
+     */
+    @Override
+    public PageInfo<FriendVo> selectFriend(PageRequest page,Long merchantId) {
+        PageHelper.startPage(page.getPageNum(), page.getPageSize());
+        List<FriendVo> friendVos = appointmentMeetDao.selectFriend(merchantId);
+        PageInfo<FriendVo> friendVoPageInfo = new PageInfo<>(friendVos);
+        return friendVoPageInfo;
+    }
+
+    /**
+     * PC端查询加我的享友数据
+     * @param merchantId
+     * @return
+     */
+    @Override
+    public PageInfo<FriendVo> selectFriendAdd(PageRequest page,Long merchantId) {
+        PageHelper.startPage(page.getPageNum(), page.getPageSize());
+        List<FriendVo> friendVs = appointmentMeetDao.selectFriendAdd(merchantId);
+        PageInfo<FriendVo> voPageInfo = new PageInfo<>(friendVs);
+        return voPageInfo;
     }
 }
