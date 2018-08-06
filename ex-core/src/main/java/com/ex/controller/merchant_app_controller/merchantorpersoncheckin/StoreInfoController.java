11e85167f3f1a99fdb646d5e39993bd25f5d65dc
王辉 <wh13552705269@163.com>
2018-08-06 16:12:58 +0800

    W

--- ex-core/src/main/java/com/ex/controller/merchant_app_controller/merchantorpersoncheckin/StoreInfoController.java
+++ ex-core/src/main/java/com/ex/controller/merchant_app_controller/merchantorpersoncheckin/StoreInfoController.java
@@ -2,14 +2,19 @@ package com.ex.controller.merchant_app_controller.merchantorpersoncheckin;
 
 import com.ex.entity.StoreInfo;
 import com.ex.service.StoreInfoService;
+import com.ex.util.FileUploadTool;
 import com.ex.util.JsonView;
 import com.ex.util.PageRequest;
+import com.ex.vo.FileEntity;
 import com.ex.vo.StoreInfoVo;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 开店铺
@@ -27,9 +32,12 @@ public class StoreInfoController {
      * @return
      */
     @RequestMapping(value = "/openStore",method = RequestMethod.POST)
-    public JsonView openStore(StoreInfo storeInfo) {
+    public JsonView openStore(StoreInfo storeInfo, MultipartFile Files, HttpServletRequest request) {
         JsonView jsonView = new JsonView();
         try {
+            FileUploadTool fileUploadTool = new FileUploadTool();
+            FileEntity entity = fileUploadTool.createFile(Files, request);//上传每个文件
+            storeInfo.setAdvertisingpicurl(entity.getPath());//拼接文件上传的地址以“,”号隔开
             int i = storeInfoService.insertStoreInfo(storeInfo);
             jsonView.setCode(JsonView.SUCCESS);
             jsonView.setMessage("开通成功!");
@@ -66,7 +74,7 @@ public class StoreInfoController {
     }
 
     /**
-     * 按条件查询店铺信息
+     * 按条件查询店铺信息j
      * @param page
      * @param storeInfo
      * @return
